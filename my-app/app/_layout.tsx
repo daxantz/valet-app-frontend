dc5f16a240389aed92f34ebb4ea0cbf5727b54a7
daxantz <daxantz@gmail.com>
2025-11-02 14:34:52 -0500

    install reacy query for data fetching

--- my-app/app/_layout.tsx
+++ my-app/app/_layout.tsx
@@ -10,47 +10,50 @@ import "@/global.css";
 import { useColorScheme } from "@/hooks/use-color-scheme";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { LocationProvider } from "@/context/LocationContext";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export const unstable_settings = {
   anchor: "(tabs)",
 };
-
+const queryClient = new QueryClient();
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <LocationProvider>
-        <ThemeProvider
-          value={colorScheme === "dark" ? DarkTheme : DefaultTheme}
-        >
-          <Stack>
-            {/* <Stack.Screen name="(tabs)" options={{ headerShown: false }} /> */}
-            <Stack.Screen
-              name="index"
-              options={{
-                headerTitle: "Location Login",
-                headerBackTitle: "Back",
-              }}
-            />
-            <Stack.Screen
-              name="employee/index"
-              options={{
-                headerTitle: "Employee Login",
-                headerBackTitle: "Back",
-              }}
-            />
-            <Stack.Screen
-              name="main/index"
-              options={{
-                headerTitle: "Dashboard",
-                headerBackTitle: "Back",
-              }}
-            />
-          </Stack>
-          <StatusBar style="auto" />
-        </ThemeProvider>
-      </LocationProvider>
+      <QueryClientProvider client={queryClient}>
+        <LocationProvider>
+          <ThemeProvider
+            value={colorScheme === "dark" ? DarkTheme : DefaultTheme}
+          >
+            <Stack>
+              {/* <Stack.Screen name="(tabs)" options={{ headerShown: false }} /> */}
+              <Stack.Screen
+                name="index"
+                options={{
+                  headerTitle: "Location Login",
+                  headerBackTitle: "Back",
+                }}
+              />
+              <Stack.Screen
+                name="employee/index"
+                options={{
+                  headerTitle: "Employee Login",
+                  headerBackTitle: "Back",
+                }}
+              />
+              <Stack.Screen
+                name="main/index"
+                options={{
+                  headerTitle: "Dashboard",
+                  headerBackTitle: "Back",
+                }}
+              />
+            </Stack>
+            <StatusBar style="auto" />
+          </ThemeProvider>
+        </LocationProvider>
+      </QueryClientProvider>
     </GestureHandlerRootView>
   );
 }
