67f91de449d4b42bc0f537c2fe4d1e8fcc26e552
daxantz <daxantz@gmail.com>
2025-11-02 12:00:13 -0500

    create stack layout for application and import styles

--- my-app/app/_layout.tsx
+++ my-app/app/_layout.tsx
@@ -1,24 +1,56 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import "react-native-reanimated";
+import "@/global.css";
+import { useColorScheme } from "@/hooks/use-color-scheme";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { LocationProvider } from "@/context/LocationContext";
 
 export const unstable_settings = {
-  anchor: '(tabs)',
+  anchor: "(tabs)",
 };
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <LocationProvider>
+        <ThemeProvider
+          value={colorScheme === "dark" ? DarkTheme : DefaultTheme}
+        >
+          <Stack>
+            {/* <Stack.Screen name="(tabs)" options={{ headerShown: false }} /> */}
+            <Stack.Screen
+              name="index"
+              options={{
+                headerTitle: "Location Login",
+                headerBackTitle: "Back",
+              }}
+            />
+            <Stack.Screen
+              name="employee/index"
+              options={{
+                headerTitle: "Employee Login",
+                headerBackTitle: "Back",
+              }}
+            />
+            <Stack.Screen
+              name="main/index"
+              options={{
+                headerTitle: "Dashboard",
+                headerBackTitle: "Back",
+              }}
+            />
+          </Stack>
+          <StatusBar style="auto" />
+        </ThemeProvider>
+      </LocationProvider>
+    </GestureHandlerRootView>
   );
 }
