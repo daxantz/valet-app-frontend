810fb5b7569951bb34d952205d5615dc7b395129
daxantz <daxantz@gmail.com>
2025-11-17 23:13:07 -0500

    add fallback view for when data length is zero

--- my-app/app/main/index.tsx
+++ my-app/app/main/index.tsx
@@ -1,10 +1,12 @@
 import { useLocation } from "@/context/LocationContext";
 import { Car } from "@/types/types";
 import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
 
-import dayjs from "dayjs";
-import { View, Text } from "react-native";
+import { Text, View } from "react-native";
 import CarBox from "@/components/CarBox";
+import Sidebar from "@/components/Sidebar";
+import EditModal from "@/components/editModal";
 
 async function fetchCars(locationId: string, entranceId: string) {
   try {
@@ -24,20 +26,41 @@ async function fetchCars(locationId: string, entranceId: string) {
 
 export default function Main() {
   const { selectedEntrance: entranceId, locationId } = useLocation();
+  const [selectedCar, setSelectedCar] = useState<Car | null>(null);
+  const [modalVisible, setModalVisible] = useState(false);
   const { data } = useQuery({
     queryKey: ["cars"],
     queryFn: () => fetchCars(locationId!, entranceId!),
   });
-  console.log(dayjs);
-
-  return (
-    <View>
-      <Text className="text-6xl">{entranceId}</Text>
-      <View className="mt-6 flex-row gap-4 p-6">
-        {data?.cars.map((car: Car) => (
-          <CarBox key={car.id} car={car} />
-        ))}
+  console.log(modalVisible);
+  const sideBarPropsValid = selectedCar && locationId && entranceId;
+  if (data?.cars.length === 0) {
+    return (
+      <View className="flex-1 items-center justify-center">
+        <Text className="text-4xl">No cars parked</Text>
       </View>
+    );
+  }
+  return (
+    <View className="flex-row">
+      <View className="mt-6  p-6 flex-row gap-4 w-[75%]">
+        {data?.cars.map((car: Car) => (
+          <CarBox key={car.id} car={car} setSelectedCar={setSelectedCar} />
+        ))}
+        <EditModal
+          setModalVisible={setModalVisible}
+          modalVisible={modalVisible}
+        />
+      </View>
+      {sideBarPropsValid && (
+        <Sidebar
+          car={selectedCar}
+          locationId={locationId}
+          entranceId={entranceId}
+          setModalVisible={setModalVisible}
+          setSelectedCar={setSelectedCar}
+        />
+      )}
     </View>
   );
 }
