f45da62e08da3bd40fd5f39643262f6bdaf36fc9
daxantz <daxantz@gmail.com>
2025-12-16 19:46:49 -0500

    add formData api to allow image files/objects to be sent to backend

--- my-app/lib/util/createCar.ts
+++ my-app/lib/util/createCar.ts
@@ -2,27 +2,37 @@ export default async function handleCreateCar(
   carData: {
     ticket: string;
     phoneNumber: string;
-    make: string;
-    color: string;
+    make?: string;
+    color?: string;
+    photos?: string[];
   },
   locationId: string,
   selectedEntrance: string
 ) {
   try {
-    console.log("Creating car with data:", carData);
+    const formData = new FormData();
+    formData.append("ticket", carData.ticket);
+    formData.append("phoneNumber", carData.phoneNumber);
+    if (carData.make) formData.append("make", carData.make);
+    if (carData.color) formData.append("color", carData.color);
+    if (carData.photos) {
+      carData.photos.forEach((photo, index) => {
+        formData.append("images", {
+          uri: photo,
+          name: `photo_${index}.jpg`,
+          type: "image/jpeg",
+        } as any);
+      });
+    }
     const res = await fetch(
-      `http://192.168.1.18:3000/v1/location/${locationId}/entrance/${selectedEntrance}/car/`,
+      `http://192.168.1.31:3000/v1/location/${locationId}/entrance/${selectedEntrance}/car/`,
       {
         method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
         credentials: "include",
-        body: JSON.stringify(carData),
+        body: formData,
       }
     );
     const data = await res.json();
-
     console.log("Create car response:", data);
   } catch (error) {
     console.error("Error creating car:", error);
